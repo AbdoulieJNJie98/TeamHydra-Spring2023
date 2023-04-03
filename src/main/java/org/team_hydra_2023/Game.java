df0480d46256ea2951a1e287b98e7fae59838448
Abdoulie J NJie <abdouliejnjie@gmail.com>
2023-04-03 18:55:05 -0400

    Added base classes needed to start developing client's game ~Abdoulie NJie

---
+++ src/main/java/org/team_hydra_2023/Game.java
@@ -0,0 +1,15 @@
+package org.team_hydra_2023;
+
+import java.util.Scanner;
+
+public class Game {
+    Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.startGame(); // call to the start game method that will initialize a run of the game
+    }
+    private void startGame() {
+
+    }
+}
---
+++ src/main/java/org/team_hydra_2023/Rooms.java
@@ -0,0 +1,4 @@
+package org.team_hydra_2023;
+
+public class Rooms {
+}
